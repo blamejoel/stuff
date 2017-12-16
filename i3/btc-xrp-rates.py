7a7017a807129ce7c694a5bcd08c9b593f0353f4
Joel Gomez <jgome043@ucr.edu>
2017-12-16 14:27:26 -0800

    adds ltc checking

--- i3/btc-xrp-rates.py
+++ i3/btc-xrp-rates.py
@@ -5,15 +5,18 @@ from sys import argv
 from requests import get
 
 btc_url = 'https://api.coinbase.com/v2/prices/BTC-USD/spot'
+ltc_url = 'https://api.coinbase.com/v2/prices/LTC-USD/spot'
 eth_url = 'https://api.coinbase.com/v2/prices/ETH-USD/spot'
 xrp_url = 'https://data.ripple.com/v2/exchange_rates/XRP/USD+rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B?live=true'
 
 try:
     btc = loads(get(btc_url).text)['data']['amount']
+    ltc = loads(get(ltc_url).text)['data']['amount']
     eth = loads(get(eth_url).text)['data']['amount']
     xrp = loads(get(xrp_url).text)['rate']
 
     print("BTC: {:.2f}".format(float(btc)), end=' ')
+    print("LTC: {:.2f}".format(float(ltc)), end=' ')
     print("ETH: {:.2f}".format(float(eth)), end=' ')
     print("XRP: {:.5f}".format(float(xrp)), end='')
 
